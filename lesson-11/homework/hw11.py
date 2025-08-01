435086d8fd20f8ab419f5772a6cce240ae6929aa
Behzod756789 <yusupjonovbehzod@gmail.com>
2025-08-01 15:58:23 +0500

    Create hw11.py

---
+++ lesson-11/homework/hw11.py
@@ -0,0 +1,47 @@
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        raise ValueError("Cannot divide by zero")
+
+
+def reverse_string(s):
+    return s[::-1]
+
+def count_vowels(s):
+    vowels = 'aeiouAEIOU'
+    return sum(1 for char in s if char in vowels)
+
+
+
+
+import math
+
+def calculate_area(radius):
+    return math.pi * radius ** 2
+
+def calculate_circumference(radius):
+    return 2 * math.pi * radius
+
+
+
+def read_file(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            return file.read()
+    except FileNotFoundError:
+        return "File not found."
+
+
+def write_file(file_path, content):
+    with open(file_path, 'w') as file:
+        file.write(content)
